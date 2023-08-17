1aa3f03c73ec409ae50f828c155f9f325241a0aa
Dev Jaiswal <jaiswaldev79@gmail.com>
2023-08-17 20:02:42 +0530

    Displaying platfrom icons

---
+++ src/components/PlatfromIcon.tsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatfromIcon = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+    iphone: MdPhoneIphone,
+  };
+  return (
+    <HStack margin={1} >
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color= 'gray.500' /> 
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatfromIcon;
